d60b7a60a8b48c658615d54659537e345f2f2f85
Wouter van Heeswijk <woutervh@gmail.com>
2018-09-17 21:52:03 +0200

    Added some mocha test cases

--- test/case-1.ts
+++ test/case-1.ts
@@ -1,10 +1,6 @@
 import * as assert from 'assert';
 import { is } from '../index';
 
-/**
- * Tests interface, boolean, string, number, heritage clauses, interface members.
- */
-
 interface Bar<V> {
     item: V;
 }
@@ -21,21 +17,62 @@ interface Foo<T, U> extends Bar<U>, Baz<number> {
 
 const isNumber = (object: any): object is number => is<number>(object);
 
-assert.strictEqual(isNumber(0), true);
-assert.strictEqual(isNumber(1), true);
-assert.strictEqual(isNumber(42), true);
-assert.strictEqual(isNumber(-1), true);
-assert.strictEqual(isNumber('0'), false);
-assert.strictEqual(isNumber('1'), false);
-assert.strictEqual(isNumber('42'), false);
-assert.strictEqual(isNumber('-1'), false);
-assert.strictEqual(is<Foo<Bar<number>, string>>({}), false);
-assert.strictEqual(is<Foo<Bar<number>, string>>({ type: {}, secondItem: {}, thirdItem: {} }), false);
-assert.strictEqual(is<Foo<Bar<number>, string>>({ item: 'string', buz: 1, type: 'cool', secondItem: { item: 2 }, thirdItem: { item: { item: true } } }), true);
-assert.strictEqual(is<Foo<Bar<number>, string>>({ item: 'text', buz: 2, type: 'cool', secondItem: { item: 3 }, thirdItem: { item: { item: false } } }), true);
-assert.strictEqual(is<boolean>(0), false);
-assert.strictEqual(is<boolean>(1), false);
-assert.strictEqual(is<boolean>('true'), false);
-assert.strictEqual(is<boolean>('false'), false);
-assert.strictEqual(is<boolean>(true), true);
-assert.strictEqual(is<boolean>(false), true);
+describe('is', () => {
+    describe('is<Foo<Bar<number>, string>>', () => {
+        it('should return false for the empty object literal', () => {
+            assert.strictEqual(is<Foo<Bar<number>, string>>({}), false);
+        });
+
+        it('should return false for an object that only partially matches', () => {
+            assert.strictEqual(is<Foo<Bar<number>, string>>({ type: {}, secondItem: {}, thirdItem: {} }), false);
+        });
+
+        it('should return true for objects that match', () => {
+            assert.strictEqual(is<Foo<Bar<number>, string>>({ item: 'string', buz: 1, type: 'cool', secondItem: { item: 2 }, thirdItem: { item: { item: true } } }), true);
+            assert.strictEqual(is<Foo<Bar<number>, string>>({ item: 'text', buz: 2, type: 'cool', secondItem: { item: 3 }, thirdItem: { item: { item: false } } }), true);
+        });
+    });
+
+    describe('is<boolean>', () => {
+        it('should return false for numbers', () => {
+            assert.strictEqual(is<boolean>(0), false);
+            assert.strictEqual(is<boolean>(1), false);
+        });
+
+        it('should return false for strings', () => {
+            assert.strictEqual(is<boolean>(''), false);
+            assert.strictEqual(is<boolean>('true'), false);
+            assert.strictEqual(is<boolean>('false'), false);
+        });
+
+        it('should return true for booleans', () => {
+            assert.strictEqual(is<boolean>(true), true);
+            assert.strictEqual(is<boolean>(false), true);
+        });
+    });
+
+    describe('is<number>', () => {
+        it('should return true for numbers', () => {
+            assert.strictEqual(isNumber(Number.NaN), true);
+            assert.strictEqual(isNumber(Number.POSITIVE_INFINITY), true);
+            assert.strictEqual(isNumber(Number.NEGATIVE_INFINITY), true);
+            assert.strictEqual(isNumber(Number.MIN_VALUE), true);
+            assert.strictEqual(isNumber(0), true);
+            assert.strictEqual(isNumber(1), true);
+            assert.strictEqual(isNumber(42), true);
+            assert.strictEqual(isNumber(-1), true);
+        });
+
+        it('should return false for strings', () => {
+            assert.strictEqual(isNumber('0'), false);
+            assert.strictEqual(isNumber('1'), false);
+            assert.strictEqual(isNumber('42'), false);
+            assert.strictEqual(isNumber('-1'), false);
+        });
+
+        it('should return false for booleans', () => {
+            assert.strictEqual(isNumber(true), false);
+            assert.strictEqual(isNumber(false), false);
+        });
+    });
+});
--- test/case-2.ts
+++ test/case-2.ts
@@ -1,13 +1,48 @@
 import * as assert from 'assert';
 import { is } from '../index';
 
-/**
- * Type alias, mapped types, object type literal.
- */
+type Bar<T> = {
+    [K in 'key']: T;
+};
 
 type Foo<T> = Pick<T, keyof T>;
 
-assert.strictEqual(is<Foo<{ field: boolean }>>({ field: true }), true);
-assert.strictEqual(is<Foo<{ field: boolean }>>({ field: false }), true);
-assert.strictEqual(is<Foo<{ field: boolean }>>({ field: 0 }), false);
-assert.strictEqual(is<Foo<{ field: boolean }>>({ field: 1 }), false);
+describe('is', () => {
+    describe('is<Foo<{ field: boolean }>>', () => {
+        it('should return true for objects with field a boolean', () => {
+            assert.strictEqual(is<Foo<{ field: boolean }>>({ field: true }), true);
+            assert.strictEqual(is<Foo<{ field: boolean }>>({ field: false }), true);
+        });
+
+        it('should return false for objects with field a number', () => {
+            assert.strictEqual(is<Foo<{ field: boolean }>>({ field: 0 }), false);
+            assert.strictEqual(is<Foo<{ field: boolean }>>({ field: 1 }), false);
+            assert.strictEqual(is<Foo<{ field: boolean }>>({ field: Number.NaN }), false);
+        });
+
+        it('should return false for objects without field', () => {
+            assert.strictEqual(is<Foo<{ field: boolean }>>({}), false);
+            assert.strictEqual(is<Foo<{ field: boolean }>>({ wrong: true }), false);
+            assert.strictEqual(is<Foo<{ field: boolean }>>({ wrong: 0 }), false);
+        });
+    });
+
+    describe('is<Bar<number>>', () => {
+        it('should return true for objects with key a number', () => {
+            assert.strictEqual(is<Bar<number>>({ key: 0 }), true);
+            assert.strictEqual(is<Bar<number>>({ key: 1 }), true);
+            assert.strictEqual(is<Bar<number>>({ key: Number.NaN }), true);
+        });
+
+        it('should return false for objects without key', () => {
+            assert.strictEqual(is<Bar<number>>({}), false);
+            assert.strictEqual(is<Bar<number>>({ wrong: true }), false);
+            assert.strictEqual(is<Bar<number>>({ wrong: 0 }), false);
+        });
+
+        it('should return false for objects with key a string', () => {
+            assert.strictEqual(is<Bar<number>>({ key: '' }), false);
+            assert.strictEqual(is<Bar<number>>({ key: 'wrong' }), false);
+        });
+    });
+});
