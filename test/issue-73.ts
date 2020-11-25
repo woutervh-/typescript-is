7c55b43648acfbac2fc4098b6a622f75b0e13909
Wouter van Heeswijk <woutervh@gmail.com>
2020-11-25 18:05:12 +0100

    Add note in test case

--- test/issue-73.ts
+++ test/issue-73.ts
@@ -2,6 +2,8 @@ import { assertType } from '../index';
 
 /* https://github.com/woutervh-/typescript-is/issues/73 */
 
+// Note: this test case is most useful when testing in combiation with "strictNullChecks: true | false"
+
 describe('assertType', () => {
     describe('assertType<string | null>', () => {
         it('should not throw an error for null', () => {
