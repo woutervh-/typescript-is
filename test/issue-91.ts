0ed97fe8559da031467e152538b5462ac2f52414
DetachHead <detachhead@users.noreply.github.com>
2021-02-27 18:33:53 +1000

    add more tests

--- test/issue-91.ts
+++ test/issue-91.ts
@@ -1,6 +1,21 @@
 import {is} from '../index';
 import * as assert from 'assert';
 
-it('template literal types', () => {
-    assert(is<`foo${string}`>('foobar'))
+
+describe('template literal types', () => {
+    describe('union types', () => {
+        type Foo = `foo${'bar' | 'baz'}`
+        it('true', () => assert(is<Foo>('foobar')))
+        it('false', () => assert(!is<Foo>('barbaz')))
+    })
+
+    describe('primitives', () => {
+        type Foo = `foo${string}bar${number}`
+        it('true', () => assert(is<Foo>('foobazbar123')))
+        it('false', () => assert(!is<Foo>('foobazbar123qux')))
+    })
+
+    describe('buncha stuff', () =>
+        assert(is<`foo${string}${number}bar${1 | 2 | 3}baz${number}asdf`>('foobar'))
+    )
 })
