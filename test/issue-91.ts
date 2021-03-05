a44dd462a5f23150cd4b86b1a9e6857895663ec3
DetachHead <detachhead@users.noreply.github.com>
2021-03-05 21:56:52 +1000

    make the tests more like the others

--- test/issue-91.ts
+++ test/issue-91.ts
@@ -4,17 +4,19 @@ import * as assert from 'assert';
 describe('template literal types', () => {
     describe('union types', () => {
         type Foo = `foo${'bar' | 'baz'}`
-        it('true', () => assert(is<Foo>('foobar')))
-        it('false', () => assert(!is<Foo>('barbaz')))
+        it('true', () => assert.deepStrictEqual(is<Foo>('foobar'), true))
+        it('false', () => assert.deepStrictEqual(is<Foo>('barbaz'), false))
     })
 
     describe('primitives', () => {
         type Foo = `foo${string}bar${number}`
-        it('true', () => assert(is<Foo>('foobazbar123')))
-        it('false', () => assert(!is<Foo>('foobazbar123qux'))) // TODO: figure out why this one causes a stack overflow when using assertType
+        it('true', () => assert.deepStrictEqual(is<Foo>('foobazbar123'), true))
+        it('false', () => assert.deepStrictEqual(is<Foo>('foobazbar123qux'), false))
     })
 
-    it('buncha stuff', () =>
-        assert(is<`foo${string}${number}bar${1 | 2 | 3}baz${number}asdf`>('foobar1bar2baz123asdf'))
+    it('unions and primitives', () =>
+        assert.deepStrictEqual(
+            is<`foo${string}${number}bar${1 | 2 | 3}baz${number}asdf`>('foobar1bar2baz123asdf'), true
+        )
     )
 })
