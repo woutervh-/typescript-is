07aac5383f01758e57ef0401de48dba97571b12b
DetachHead <detachhead@users.noreply.github.com>
2021-03-05 21:57:13 +1000

    add key types tests

--- test/issue-91.ts
+++ test/issue-91.ts
@@ -19,4 +19,18 @@ describe('template literal types', () => {
             is<`foo${string}${number}bar${1 | 2 | 3}baz${number}asdf`>('foobar1bar2baz123asdf'), true
         )
     )
+    describe('key types', () => {
+        it('primitives get resolved to strings', () =>
+            assert.deepStrictEqual(is<{ [K in `foo${string}`]: any }>({foo: 'bar', baz: 'qux'}), true)
+        )
+        describe('unions', () => {
+            type Foo = { [K in `foo${'bar' | 'baz'}`]: any }
+            it('true', () =>
+                assert.deepStrictEqual(is<Foo>({foobar: 'bar', foobaz: 'qux'}), true)
+            )
+            it('false', () =>
+                assert.deepStrictEqual(is<Foo>({foo: 'bar', baz: 'qux'}), false)
+            )
+        })
+    })
 })
